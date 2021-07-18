cfb8488a0a0dcd6664d41d1838c760a1be0bb5e8
Michael Wood <esiotrot@gmail.com>
2021-07-18 16:04:27 +0200

    Copy flat-list-mvcp Example and install expo-dev-client

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import 'expo-dev-client';
+import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import ScrollViewExample from './src/ScrollViewExample';
+import FlatListExample from './src/FlatListExample';
+
+const App = () => {
+  return <FlatListExample />;
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Expo flat-list-mvcp
+
+Copied from [flat-list-mcvp/Example](https://github.com/GetStream/flat-list-mvcp/tree/main/Example) and tweaked to work with Expo
